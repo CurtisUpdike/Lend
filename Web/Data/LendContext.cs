1da4f08999344285cdcb70ae086859ef0b28e4b7
Curtis Updike <curtis@updike.dev>
2023-04-27 22:05:02 -0400

    User model

---
+++ Web/Data/LendContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data;
+
+public class LendContext : IdentityDbContext<User>
+{
+    public LendContext(DbContextOptions<LendContext> options) : base(options) { }
+}
---
+++ Web/Models/User.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Web.Models;
+
+public class User : IdentityUser
+{
+}
